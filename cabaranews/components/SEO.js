b4d7809cf000d0df3873dcde55a25531eb18ece1
milkyway877 <wmilky877@gmail.com>
2026-06-24 15:37:38 +0200

    Add favicon and 9 new articles

--- cabaranews/components/SEO.js
+++ cabaranews/components/SEO.js
@@ -44,7 +44,10 @@ export default function SEO({
       {/* Misc */}
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta charSet="utf-8" />
-      <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" href="/favicon.ico" sizes="any" />
+      <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+      <link rel="apple-touch-icon" href="/icon-192.png" />
+      <link rel="manifest" href="/site.webmanifest" />
     </Head>
   )
 }
